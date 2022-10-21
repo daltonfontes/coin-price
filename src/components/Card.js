11a537453b7ba87caaabbdd9d236fca80f89d928
daltonfontes <fontes.dalton@hotmail.com>
2022-10-20 23:04:20 -0300

    feat : ajuste do tema

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,18 +5,27 @@ export const Card = () => {
     const [data, setData] = useState([]);
 
     useEffect(() => {
-        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
+        fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&order=market_cap_desc&per_page=10&page=1&sparkline=false`)
             .then(response => response.json())
             .then(json => setData(json))
+            .catch(error => console.log(error));
     }, []);
-
     console.log(data);
 
     return (
-        <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:flex-shrink-0" src={data[0].image}/>
-            <h1>{data[0].name}</h1>
-            <h2>{data[0].current_price}</h2>
+        <div className="grid gap-4 grid-cols-2">
+            {data.map((item) => (
+                <figure className="max-w-sm mx-auto shadow-md" key={item.id}>
+                    <div className="text-center">
+                        <img className="rounded-full w-32 mb-4 mx-auto" src={item.image} alt={item.name} />
+                    </div>
+                    <div className="p-6">
+                        <h3 className="text-gray-900 text-xl font-medium mb-2">{item.name}</h3>
+                        <p className="text-gray-700 text-base mb-4">{item.symbol}</p>
+                        <p className="text-gray-700 text-base mb-4">R${item.current_price}</p>
+                    </div>
+                </figure>
+            ))}
         </div>
-    )
-}
+    );
+};
